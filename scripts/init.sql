357c431b4a1705cf6402a3da932b1e48ad46e617
Blake <blakekelley127@gmail.com>
2020-01-02 21:21:19 -0700

    Removing uneeded scripts

--- scripts/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Device (
-    id INT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description VARCHAR(150),
-    mac VARCHAR(17) NOT NULL,
-    ip VARCHAR(15) NOT NULL,
-    type VARCHAR(50) NOT NULL
-);
